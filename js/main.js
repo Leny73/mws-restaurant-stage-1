c9d2b6ff6fb28b36ec1a20ad3c94080a6300e96f
Leny73 <lbranzalov95@gmail.com>
2018-08-25 09:08:30 +0300

    changing service worker]

--- js/main.js
+++ js/main.js
@@ -210,8 +210,9 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 } */
 
-if(navigator.serviceWorker){
-navigator.serviceWorker.register('/js/sw2.js').then(function(){
+function registerServiceWorker(){
+  if(!navigator.serviceWorker) return;
+navigator.serviceWorker.register('/js/sw.js').then(function(){
   console.log('Registration worked!');
 }).catch(function(){
   console.log('Registration failed');
